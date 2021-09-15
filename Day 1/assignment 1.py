6b609f75c553f462256db8ef9c6ba68e1d1f4d90
Amisha17-b <80813453+Amisha17-b@users.noreply.github.com>
2021-09-15 21:14:08 +0530

    Add files via upload

---
+++ Day 1/assignment 1.py
@@ -0,0 +1,12 @@
+a=10
+b=10
+c=20
+
+print(a is b)
+print(a is c)
+print(b is c)
+
+x =["apple"]
+y =["orange"]
+
+print(x is y)
---
+++ Day 1/assignment 3.py
@@ -0,0 +1,8 @@
+x = 10
+y = 10.22
+str="apple"
+
+print (x,y,str)
+print(type(x))
+print(type(y))
+print(type(str))
